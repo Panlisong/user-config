9a5e697bde9f62cb1ed3e6349af35cd522f4cfeb
Song <panlisong2010@qq.com>
2025-04-07 16:09:19 +0800

    feat: change keymap for searching in buffers

--- nvim/lua/keymaps/telescope.lua
+++ nvim/lua/keymaps/telescope.lua
@@ -1,8 +1,9 @@
 -- set key map and key map options
 local keymap = vim.api.nvim_set_keymap
-local opt = { noremap = true, silent = true }
 
-keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opt)
-keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", opt)
-keymap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opt)
-keymap("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opt)
+local normal_opt = { noremap = true, silent = true }
+
+keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", normal_opt)
+keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", normal_opt)
+keymap("n", "<leader>fb", "<cmd>Telescope live_grep grep_open_files=true<CR>", normal_opt)
+keymap("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", normal_opt)
